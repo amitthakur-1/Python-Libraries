f75369f0fe7826c3b46b3d2be7126bc302ecaf6e
Amit Thakur <amitthakur70072@gmail.com>
2026-04-09 18:21:30 +0530

    Add files via upload

---
+++ Numpy d1.py
@@ -0,0 +1,40 @@
+
+# arr_1d=np.array([1,2,3,4])
+# print(arr_1d)
+# print(type(arr_1d))
+# print(arr_1d.ndim)....check arr type
+# print(arr_1d.size)....size
+
+# Arr_2d=np.array([[1,2,3],[5,6,7]])
+# print(Arr_2d)
+# print(Arr_2d.ndim)
+# print(Arr_2d.size)
+# print(Arr_2d.shape)
+# print(type(Arr_2d))
+
+
+# import numpy as np
+# mx_1s = np.array([[[1,1,1],[1,1,1],[1,1,1]]])
+# print(mx_1s)
+# print(np.ones(5))
+# print(mx_1s.dtype)
+# print(np.ones((3,4), dtype=int))
+# mx_1s = np.zeros((4,6), dtype=bool)
+# print(mx_1s)
+
+# operations on numpy
+# em_mx=np.empty
+# import numpy as np
+# c=np.array([1,2,3,4])
+# print(c+2)
+# print(c-2)
+# print(c*2)
+# print(c/2)
+# print(c%2)
+# print(c**2)
+# # print(np.sqrt(c))
+# print(np.sum(c))
+# print(np.mean(c))
+# # print(np.max(c))
+# # print(np.min(c))
+
