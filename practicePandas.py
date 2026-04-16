6541cbc54cd00d563668a41041a21f3ab161f45d
Amit Thakur <amitthakur70072@gmail.com>
2026-04-16 11:20:46 +0530

    Update practicePandas.py

--- practicePandas.py
+++ practicePandas.py
@@ -193,6 +193,12 @@ import pandas as pd
 # print(df.describe())
 # print(df.loc())
 
+# arraypd
+# import numpy as np
+# import pandas as pd
 
+# data=np.array([[1,"varun",23],[2,"aman",23],[3,"dhir",23]])
+# df=pd.DataFrame(data,columns=["id","name","age"])
+# print(df)
 
 
