c7affdf70fcbd9800c19512ca969ec7ae1575c51
Amit Thakur <amitthakur70072@gmail.com>
2026-04-16 11:18:32 +0530

    Update practicePandas.py

--- practicePandas.py
+++ practicePandas.py
@@ -141,6 +141,12 @@ import pandas as pd
 # print("division")
 # print(s/2)
 
+# list with pd
+# import pandas as pd
+# data=[["amit",31,30000],["nitin",122,23223231],["raman",23435,53534253]]
+# df=pd.DataFrame(data)
+# print(df)
+# print(type(df))
 
 # s=pd.Series([12,23,34])
 # d=pd.Series([44,5,21])
